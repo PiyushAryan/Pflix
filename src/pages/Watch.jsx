6ad1e37b2100eb65d3a68738a1ffbe17bb355dd7
Piyush Aryan <piyusharyan81@gmail.com>
2024-12-30 19:48:23 +0530

    fix:revert to normal youtube player

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -7,10 +7,9 @@ import 'video-react/dist/video-react.css';
 function Watch() {
     return (
         <>
-            <div className='h-screen w-screen bg-black bg-opacity-70 flex flex-row gap-8 m-5'>
-            <Link to='/Home'><FaArrowLeft className='text-white w-10 h-10' /></Link>
-            {/* <h1 className='text-white flex justify-center items-center'> Hello Watch </h1> */}
-            <iframe className="border- h-screen flex justify-center content-center" width="100%" height="100%" src="https://www.youtube.com/embed/no4awCAdOBU" frameBorder="0"  allowfullscreen></iframe>
+            <div className='h-screen w-screen bg-black bg-opacity-70 relative'>
+            <Link to='/Home' className="absolute top-4 left-5"><FaArrowLeft className='text-white w-10 h-10' /></Link>
+            <iframe className="border-none justify-center content-center" width="100%" height="100%" src="https://www.youtube.com/embed/no4awCAdOBU" allowfullscreen></iframe>
             </div>
 
         </>
