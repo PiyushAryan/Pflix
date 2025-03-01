64e3a30c933e978451af660696c5e84ef251d09d
Piyush Aryan <piyusharyan81@gmail.com>
2025-03-02 02:27:10 +0530

    added new tiles in skills

--- src/components/skillgrid.jsx
+++ src/components/skillgrid.jsx
@@ -2,17 +2,18 @@ import { useState } from "react";
 
 const skills = [
   { name: "Next.js", projects: 1, color: "bg-gray-300", logo: "nextjs.svg" },
-  { name: "React", projects: 3, color: "bg-gray-900", logo: "react.svg" },
+  { name: "React", projects: 5, color: "bg-gray-900", logo: "react.svg" },
   { name: "Vercel", projects: 2, color: "bg-white", logo: "vercel.svg" },
   { name: "Tailwind", projects: 12, color: "bg-[#06B6D4]", logo: "tailwind.svg" },
-  { name: "Vite", projects: 6, color: "bg-purple-100", logo: "vite.svg" },
+  { name: "Vite", projects: 5, color: "bg-purple-100", logo: "vite.svg" },
   { name: "Docker", projects: 2, color: "bg-[#1D63ED]", logo: "docker-mark-white.svg" },
-  { name: "Firebase", projects: 7, color: "bg-[#303030]", logo: "firebase.svg" },
+  { name: "Firebase", projects: 1, color: "bg-[#303030]", logo: "firebase.svg" },
   { name: "Leaflet", projects: 1, color: "bg-green-100", logo: "leaflet.png" },
   { name: "AWS Lambda", projects: 1, color: "bg-[#D9660B]", logo: "Lambda.svg" },
   { name: "AWS S3", projects: 2, color: "bg-green-200", logo: "S3.svg" },
   { name: "AWS Amplify", projects: 1, color: "bg-red-200", logo: "Amplify.svg" },
   { name: "Amazon API Gateway", projects: 1, color: "bg-pink-200", logo: "API Gateway.svg" },
+  { name: "TeamCity", projects: 0, color: "bg-slate-900", logo: "TeamCity.svg" },
 ];
 
 export default function SkillGrid() {
